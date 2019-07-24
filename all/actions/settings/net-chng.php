850aa8a5de119a4eb0e8bf0a27eb9f3b53948c70
Devon Shusterman <djs79@duke.edu>
2019-07-24 13:58:57 -0400

    grab

--- all/actions/settings/net-chng.php
+++ all/actions/settings/net-chng.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Settings</title>
-		<link rel="stylesheet" href="assets/stylesheets/main.css">
+		<link rel="stylesheet" href="../../assets/stylesheets/main.css">
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=EB+Garamond:400,400i,700" rel="stylesheet">
